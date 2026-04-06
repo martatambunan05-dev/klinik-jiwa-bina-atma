7ca28579a46990ffd03f9706574347dceece1d60
martatambunan05-dev <martatambunan05@gmail.com>
2026-04-06 22:13:33 +0700

    Create User_model.php with user methods

---
+++ application/models/User_model.php
@@ -0,0 +1,23 @@
+<?php
+
+class User_model {
+
+    public function get_user() {
+        // Logic to get user data
+    }
+
+    public function cek_username($username) {
+        // Logic to check if username exists
+    }
+
+    public function insert_user($userData) {
+        // Logic to insert new user data
+    }
+
+    public function get_user_by_id($id) {
+        // Logic to get user by ID
+    }
+
+}
+
+?>
